70897daf9c0b0fcb7b82a2b867c9419cd28e8655
Somesh Kale <gkale.somesh@gmail.com>
2018-08-15 19:38:56 +0530

    Update smile_detector.py

--- smile_detector.py
+++ smile_detector.py
@@ -12,13 +12,16 @@ smile_cascade=cv2.CascadeClassifier('haarcascade_smile.xml')
 
 def detect(gray, frame):
     faces=face_cascade.detectMultiScale(gray, 1.3, 5)
+    #for face detection
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         r_gray=gray[y:y+h,x:x+w]
         r_color=frame[y:y+h,x:x+w]
+        #for eyes detection
         eyes=eye_cascade.detectMultiScale(r_gray, 1.1, 15)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(r_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        #for smile detection
         smiles=smile_cascade.detectMultiScale(r_gray, 1.7, 35)
         for (sx,sy,sw,sh) in smiles:
             cv2.rectangle(r_color,(sx,sy),(sx+sw,sy+sh),(0,0,255),2)
